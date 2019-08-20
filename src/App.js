9b350a4880c9d6f4e874db435b76b0ecd097ab9b
James Wilson <jwilsoncalderdale@gmail.com>
2019-08-21 00:50:12 +0100

    add lazy loading

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.scss';
-import TrainTrack from './TrainTrack';
-import Text from './Text';
+const TrainTrack = React.lazy(() => import('./TrainTrack'));
+const Text = React.lazy(() => import('./Text'));
+
+const Fallback = (
+  <div className="fallback">
+    <p>Just wait... It will be good.</p>
+  </div>
+)
+
 
 const App = () => (
   <div className="App">
-    <TrainTrack />
-    <Text />
+    <React.Suspense fallback={Fallback}>
+      <TrainTrack />
+      <Text />
+    </React.Suspense>
   </div >
 );
 
