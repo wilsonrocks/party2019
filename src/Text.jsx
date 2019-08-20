1be10cebfbc5808f66332afc5a6a339b1dc43982
James Wilson <jwilsoncalderdale@gmail.com>
2019-08-20 23:10:54 +0100

    add post it notes

--- src/Text.jsx
+++ src/Text.jsx
@@ -1,44 +1,61 @@
-import React, { useState, Fragment } from 'react';
-
-const content = [
-  <Fragment>
-    <p className="invitee">{window.location.pathname.slice(1).split('-').join(' ')}</p>
-    <p>please come to</p>
-    <p>Evelyn and Amos' party</p>
-    <p>Saturday 5 October</p>
-    <p>2pm - 5pm</p>
-  </Fragment>,
-
-  <Fragment>
-    <p>Ravensprings Park</p>
-    <a href="https://www.bhme.co.uk/">bhme.co.uk</a>
-    <p>Cawcliffe Road</p>
-    <p>Brighouse</p>
-    <p>HD6 2HP</p>
-  </Fragment>,
-
-  <Fragment>
-    <p>Food for children</p>
-    <p>Tea and cake for grown-ups</p>
-    <p>Bring wellies and waterproofs</p>
-  </Fragment>,
-];
-
+import React from 'react';
 
 const Text = () => {
-  const [page, setPage] = useState(0);
+  const invitee = window.location.pathname.slice(1).split('-').join(' ');
+
   return (
     <div className="text-wrapper">
-      {page > 0 ? <button type="button" onClick={() => setPage(page - 1)} >&lt;</button> : <span />}
-      <div className="text">
-        {content[page]}
-      </div>
-      {page < content.length - 1 ? <button type="button" onClick={() => setPage(page + 1)} >&gt;</button> : <span />}
+      <section>
+        <div>
+          {invitee && <p className="invitee">Dear {window.location.pathname.slice(1).split('-').join(' ')}</p>}
+          <h1>It&apos;s Party Time!</h1>
+          <p>with</p>
+          <h2>Evelyn and Amos</h2>
+        </div>
+      </section>
 
+      <section>
+        <div>
+          <h1>Ravensprings Park</h1>
+          <h2>miniature railway</h2>
+        </div>
+      </section>
 
+      <section>
+        <div>
+          <h1>Saturday 5 October</h1>
+          <h2>2 - 5pm</h2>
+        </div>
+      </section>
+
+      <section>
+        <div>
+          <p>Food for children</p>
+          <p>Tea and cake for grown-ups</p>
+          <p>Bring wellies and waterproofs</p>
+        </div>
+      </section>
+
+      <section>
+        <div>
+          <p>Ravensprings Park</p>
+          <p>Cawcliffe Road</p>
+          <p>Brighouse</p>
+          <p>HD6 2HP</p>
+          <a href="https://www.bhme.co.uk/">bhme.co.uk</a>
+        </div>
+      </section>
+
+      <section>
+        <div>
+          <h1>RSVP</h1>
+          <p>Shelley: 07761901734</p>
+          <p>James: 07814509239</p>
+        </div>
+      </section>
 
     </div>
-  );
-};
+  )
+}
 
 export default Text;
